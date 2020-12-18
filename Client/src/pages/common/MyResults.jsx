1de1b85992150ec1d7f0eee264928ee921fd6e87
KrzysztofDÄabrowski <krzysztof.j.w.dabrowski@gmail.com>
2020-12-18 14:35:09 +0100

    Client fetches JSON from API

--- Client/src/pages/common/MyResults.jsx
+++ Client/src/pages/common/MyResults.jsx
@@ -1,5 +1,42 @@
-import React from "react";
+import React, { Component } from 'react';
 
-export default function MyResults() {
-  return <div>Moje wyniki</div>;
+class MyResults extends Component {
+  async componentDidMount() {
+
+    try{
+      let res = await fetch(
+        "https://localhost:44301/api/Result/Get",
+        {
+          method:"get",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      let listOfRes = await res.json();
+      console.log(listOfRes.results);
+    }
+
+    catch(e){
+      console.log(e);
+    }
+   
+  }
+  
+  render(){
+    return (
+      <div className="main-container">
+        <div className="padded-container">
+  
+  
+  
+        </div>
+      </div>
+    );
+  }
+
 }
+
+export default MyResults;
