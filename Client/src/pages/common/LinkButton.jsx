8bbfbf96302e47db215baf639f23195b804c1cf7
KrzysztofDÄabrowski <krzysztof.j.w.dabrowski@gmail.com>
2020-12-20 14:32:40 +0100

    Result list - first draft

--- Client/src/pages/common/LinkButton.jsx
+++ Client/src/pages/common/LinkButton.jsx
@@ -1,15 +1,9 @@
-import React, {useState} from "react"
+import React, { useState } from "react";
 
 export default function LinkButton() {
-
-
-
   return (
     <div className="link-btn-container">
-      <button className="link-btn">
-        {/* {text} */}
-      </button>
-
+      <button className="link-btn">{/* {text} */}</button>
     </div>
   );
 }
--- Client/src/pages/common/MyResults.jsx
+++ Client/src/pages/common/MyResults.jsx
@@ -1,42 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class MyResults extends Component {
-  async componentDidMount() {
-
-    try{
-      let res = await fetch(
-        "https://localhost:44301/api/Result/Get",
-        {
-          method:"get",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-        }
-      );
+  async componentDidMount(props) {
+    try {
+      let res = await fetch("https://localhost:44301/api/Result/Get", {
+        method: "get",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
 
       let listOfRes = await res.json();
-      console.log(listOfRes.results);
-    }
+      // console.log(listOfRes.results);
 
-    catch(e){
+      this.listItems = listOfRes.results.map((result) => (
+        <li className="result-list-item" key={result.idResult.toString}>
+          {result.technician}
+        </li>
+      ));
+      // console.log(listItems);
+
+    } catch (e) {
       console.log(e);
     }
-   
   }
-  
-  render(){
+
+  render() {
     return (
       <div className="main-container">
         <div className="padded-container">
-  
-  
-  
+          <ul className="result-list">{this.listItems}</ul>
         </div>
       </div>
     );
   }
-
 }
 
 export default MyResults;
